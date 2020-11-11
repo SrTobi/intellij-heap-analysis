930044e373c4af39fef7fcf7248b909d3599b703
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2020-11-11 20:25:49 +0100

    restructured code

--- src/main/scala/com/github/srtobi/ideaheapanalyzer/Main.scala
+++ src/main/scala/com/github/srtobi/ideaheapanalyzer/Main.scala
@@ -10,65 +10,22 @@ import scala.collection.mutable
 import scala.jdk.CollectionConverters._
 
 object Main {
-  sealed trait Item
-  case class ArrayItem(index: Int) extends Item
-  case class FieldItem(field: Field) extends Item
-  case object UnknownItem extends Item
+  /////////////////////////////////////////////////////////////////////////////////////
+  ////////////////// Adjust these functions to suit your needs ////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////
+  def shouldBeStartOfPath(instance: Instance): Boolean =
+    instance.getJavaClass.getName.contains(".scala")
 
-  implicit class InstanceOps(private val instance: Instance) {
-    assert(instance != null)
+  def filterPathFromAfterStartToGcRoot(path: Seq[Instance]): Boolean =
+    !path.exists(_.getJavaClass.getName.contains(".scala"))
 
-    def outgoingReferences(isSpecial: (FieldValue, Instance) => Boolean = (_, _) => false): Iterator[Instance] = instance match {
-      case array: ObjectArrayInstance => array.getValues.iterator().asScala.filterNot(_ == null)
-      case inst: Instance =>
-        (inst.getFieldValues.iterator().asScala ++
-          inst.getStaticFieldValues.iterator().asScala)
-          .flatMap(_.outgoungReference(isSpecial(_, instance))) /*++
-            Iterator(inst.getJavaClass.getClassLoader)*/
-    }
+  def ignoreInstance(inst: Instance): Boolean =
+    inst.getJavaClass.getName.contains("cl.PluginClassLoader")
 
-    def fieldPointingTo(target: Instance): Item = instance match {
-      case array: ObjectArrayInstance =>
-        val index = array.getValues.asScala.indexOf(target)
-        if (index >= 0) ArrayItem(index)
-        else UnknownItem
-      case inst: Instance =>
-        (inst.getFieldValues.iterator().asScala ++
-          inst.getStaticFieldValues.iterator().asScala)
-          .find(_.outgoungReference().contains(target))
-          .map(_.getField)
-          .map(FieldItem)
-          .getOrElse(UnknownItem)
-    }
 
-    def pathToGc: Seq[Instance] =
-      Iterator.unfold(instance) {
-        cur =>
-          assert(cur.getNearestGCRootPointer != null)
-          Option(cur.getNearestGCRootPointer).filter(_ != cur).map(x => x -> x)
-      }.toSeq
-
-  }
-
-  implicit class ValueOps(private val field: Value) extends AnyVal {
-    def outgoungReference(isSpecial: FieldValue => Boolean = _ => false): Option[Instance] = field match {
-      case item: ArrayItemValue => Option(item.getInstance())
-      case objField: ObjectFieldValue if !isSpecial(objField) => Option(objField.getInstance())
-      case _ => None
-    }
-  }
-
-  def findHProf(): File = {
-    val profileFiles = Paths.get(System.getProperty("user.home")).toFile
-      .listFiles()
-      .filter(_.getPath.endsWith(".hprof"))
-      .sortBy(_.getName)
-
-    println("Found head dumps:")
-    profileFiles.foreach(println)
-    profileFiles.lastOption.getOrElse(throw new Exception("Couldn't find any dumps in home directory"))
-  }
-
+  /////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////// And run ///////////////////////////////////////////
+  /////////////////////////////////////////////////////////////////////////////////////
   def main(args: Array[String]): Unit = {
     val heap: Heap = HeapFactory.createHeap(findHProf())
     val isSpecialReference = new SpecialReferenceRecognizer(heap).isSpecialReference _
@@ -77,7 +34,7 @@ object Main {
     val parentMap = mutable.Map.empty[Instance, Instance]
 
     def addToUnprocessed(inst: Instance, from: Instance = null): Unit = {
-      if (inst.getJavaClass.getName.contains("cl.PluginClassLoader"))
+      if (ignoreInstance(inst))
         return
 
       if (reachables.add(inst)) {
@@ -121,11 +78,11 @@ object Main {
     var n = 0
     reachables
       .iterator
-      .filter(_.getJavaClass.getName.contains(".scala"))
+      .filter(shouldBeStartOfPath)
       //.filterNot(reachables.contains)
       .foreach { inst =>
         val path = pathToGcRoot(inst)
-        if (!path.exists(_.getJavaClass.getName.contains(".scala"))) {
+        if (filterPathFromAfterStartToGcRoot(path)) {
           val fullPath = inst :: path
           val fieldInInst = makeFieldPointingToMap(fullPath)
           n += 1
@@ -187,6 +144,17 @@ object Main {
     }
   }
 
+  def findHProf(): File = {
+    val profileFiles = Paths.get(System.getProperty("user.home")).toFile
+      .listFiles()
+      .filter(_.getPath.endsWith(".hprof"))
+      .sortBy(_.getName)
+
+    println("Found head dumps:")
+    profileFiles.foreach(println)
+    profileFiles.lastOption.getOrElse(throw new Exception("Couldn't find any dumps in home directory"))
+  }
+
   def makeFieldPointingToMap(path: List[Instance]): Map[Instance, Item] =
     path.zip(path.tail).map {
       case (to, from) => from -> from.fieldPointingTo(to)
@@ -217,5 +185,53 @@ object Main {
       val intersection = (aRest intersect bRest).size.toFloat
       (intersection / math.min(aRest.size, bRest.size)) > 0.5
     } else false
+  }
+
+  sealed trait Item
+  case class ArrayItem(index: Int) extends Item
+  case class FieldItem(field: Field) extends Item
+  case object UnknownItem extends Item
+
+  implicit class InstanceOps(private val instance: Instance) {
+    assert(instance != null)
+
+    def outgoingReferences(isSpecial: (FieldValue, Instance) => Boolean = (_, _) => false): Iterator[Instance] = instance match {
+      case array: ObjectArrayInstance => array.getValues.iterator().asScala.filterNot(_ == null)
+      case inst: Instance =>
+        (inst.getFieldValues.iterator().asScala ++
+          inst.getStaticFieldValues.iterator().asScala)
+          .flatMap(_.outgoungReference(isSpecial(_, instance))) /*++
+            Iterator(inst.getJavaClass.getClassLoader)*/
+    }
+
+    def fieldPointingTo(target: Instance): Item = instance match {
+      case array: ObjectArrayInstance =>
+        val index = array.getValues.asScala.indexOf(target)
+        if (index >= 0) ArrayItem(index)
+        else UnknownItem
+      case inst: Instance =>
+        (inst.getFieldValues.iterator().asScala ++
+          inst.getStaticFieldValues.iterator().asScala)
+          .find(_.outgoungReference().contains(target))
+          .map(_.getField)
+          .map(FieldItem)
+          .getOrElse(UnknownItem)
+    }
+
+    def pathToGc: Seq[Instance] =
+      Iterator.unfold(instance) {
+        cur =>
+          assert(cur.getNearestGCRootPointer != null)
+          Option(cur.getNearestGCRootPointer).filter(_ != cur).map(x => x -> x)
+      }.toSeq
+
+  }
+
+  implicit class ValueOps(private val field: Value) extends AnyVal {
+    def outgoungReference(isSpecial: FieldValue => Boolean = _ => false): Option[Instance] = field match {
+      case item: ArrayItemValue => Option(item.getInstance())
+      case objField: ObjectFieldValue if !isSpecial(objField) => Option(objField.getInstance())
+      case _ => None
+    }
   }
 }
